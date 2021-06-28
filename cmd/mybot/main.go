a07a366771a65794890b1950dffab29ffaf236a7
Oleg <dmitrievoleg96@gmail.com>
2021-06-29 01:15:54 +0300

    wh fixed

--- cmd/mybot/main.go
+++ cmd/mybot/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"mybot/pkg/handler"
 	"mybot/pkg/mybot"
-	"time"
+	"net/http"
+	"os"
 )
 
 var (
 	PericBotToken = "1887809470:AAGvUYu2u-H1DQd16Si0Dpag3-sME_2HjwY"
 	WebHookURL    = "https://peric-telegram-bot.herokuapp.com"
 )
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+	log.Println("In: GETALL")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("ERR: ", err)
+		return
+	}
+	log.Println(body)
+}
 
 func main() {
 	bot, err := mybot.NewBot(PericBotToken)
@@ -21,5 +33,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	time.Sleep(20 * time.Minute)
+	http.HandleFunc("/", GetAll)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
